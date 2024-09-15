5fe901f32e7eb895ac20171bebe15a87ebc337ee
Pyx <darkpixlz@proton.me>
2024-09-15 18:06:49 -0400

    App server v2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Webserver API
 from fastapi import FastAPI, Response, Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 
 # Typing
@@ -17,24 +18,81 @@ from urllib.request import urlretrieve
 # Misc
 import platform
 import httpx
+from time import time
 import orjson as json
 from sys import version
-from models import RatingPayload
+from collections import defaultdict
+
+from modules.database import db
+from modules.models import RatingPayload
 
 app = FastAPI()
-app_server_version = "1.0"
-roblox_lock = True
+app_server_version = "2.0"
+
+api_lock = False
+enable_sessions = False
+roblox_lock = not "zen" in platform.release()
+
+rate_limit_reqs = 10
+rate_limit_reset = 120
+rate_limit_max_incidents = 3
+
+limited_ips = defaultdict(list)
+mem_incidents = defaultdict(list)
+mem_blocked_ips = defaultdict(list)
+
+blocked_users = db.get("__BLOCKED_USERS__", db.API_KEYS)
+blocked_games = db.get("BLOCKED__GAMES__", db.API_KEYS)
 
 sys_string = f"{platform.system()} {platform.release()} ({platform.version()})"
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: FunctionType) -> Response:
-        if roblox_lock and not request.headers.get("Roblox-id") and not request.url == "http://0.0.0.0:7010":
+        if roblox_lock and not request.headers.get("Roblox-id") and not request.url == "http://0.0.0.0:7010/":
             return JSONResponse({"code": 400, "message": "This App Server is only accepting requests from Roblox game servers."}, status_code=400)
+        
+        if api_lock and not request.headers.get("X-Administer-Key"):
+            return JSONResponse({"code": 400, "message": "A valid API key must be used."}, status_code=400)
+        
+        elif api_lock and not db.get(request.headers.get("X-Administer-Key"), db.API_KEYS):
+            return JSONResponse({"code": 400, "message": "This API key is not valid."}, status_code=400)
+        
+        elif api_lock:
+            api_key_data = db.get(request.headers.get("X-Administer-Key"), db.API_KEYS)
+
+            if api_key_data.disabled or api_key_data.registered_to in blocked_users or api_key_data.registered_game in blocked_games:
+                return JSONResponse({"code": 400, "message": "Your API key has been disabled due to possible abuse. To reach out to the API team please join the discord and make a ticket (/discord)."}, status_code=400)
+        
+        if enable_sessions and not request.headers.get("X-Administer-Session"):
+            return JSONResponse({"code": 400, "message": "A valid session token is required."}, status_code=400)
 
         return await call_next(request)
     
+class RateLimiter(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: FunctionType) -> Response:
+        cf_ip = request.headers.get("CF-Connecting-IP")
+
+        if cf_ip in mem_blocked_ips:
+            return Response(status_code=400, content="Sorry, you have been blocked. If you believe this is in error please reach out.")
+
+        if not cf_ip:
+            # development install?
+            return await call_next(request)
+
+        limited_ips[cf_ip] = [
+            timestamp for timestamp in limited_ips[cf_ip]
+            if timestamp > time() - rate_limit_reset
+        ]
+
+        if len(limited_ips[cf_ip]) >= rate_limit_reqs:
+            return Response(status_code=429, content="Too many requests. Try again later. Do NOT refresh this page or else you will be blocked.")
+
+        limited_ips[cf_ip].append(time())
+        response = await call_next(request)
+        return response
+    
 app.add_middleware(AuthMiddleware)
+app.add_middleware(RateLimiter)
 
 def request_app(app_id):
     app_path = (Path("data/apps") / f"{app_id}.json")
@@ -42,7 +100,8 @@ def request_app(app_id):
     if not app_path.is_file():
         return None
     
-    return json.loads(app_path.read_text())
+    #return json.loads(app_path.read_text())
+    return db.get(app_id, db.APPS)
 
 @app.get("/")
 async def root():
@@ -56,7 +115,8 @@ async def verify_administer_server():
         "engine": version,
         "system": sys_string,
         "app_server_api_version": app_server_version,
-        "target_administer_version": "1.0"
+        "target_administer_version": "1.0",
+        "known_apps": len(db.get_all(db.APPS))
     }, status_code=200)
 
 @app.get("/app/{appid}")
@@ -65,6 +125,7 @@ async def get_app(appid: str):
         app = request_app(appid)
 
         if app == None: raise FileNotFoundError
+
         return JSONResponse(app, status_code = 200)
     
     except (FileNotFoundError, OSError):
@@ -76,18 +137,17 @@ async def get_app(appid: str):
 
 @app.post("/rate/{app_id}")
 async def rate_app(req: Request, app_id: str, payload: RatingPayload):
-    place_file = (Path("data/places") / f"{req.headers.get("Roblox-Id")}.json")
+    place = db.get(req.headers.get("Roblox-Id"), db.PLACES)
     rating = payload.Rating
     
-    if not place_file.is_file():
+    if not place:
         return JSONResponse({
             "code": 400,
             "message": "bad-request",
-            "user_facing_message": "Your place isn't in the database. Please try installing something and try again later."
+            "user_facing_message": "We can't find your game."
         }, status_code=400)
-    place_file = json.loads(place_file.read_text())
 
-    if int(app_id) not in place_file["Installs"]:
+    if int(app_id) not in place["apps"]:
         return JSONResponse({
             "code": 400,
             "message": "bad-request",
@@ -102,16 +162,16 @@ async def rate_app(req: Request, app_id: str, payload: RatingPayload):
             "user_facing_message": "Could not find that app. Was it deleted?"
         }, status_code=404)
     
-    if app_id in place_file["Ratings"]:
-        place_file["Ratings"][app_id] = None
+    if app_id in place["ratings"]:
+        place["ratings"][app_id] = None
         app[rating and "AppLikes" or "AppDislikes"] -= 1
         print("Overwriting rating.")
     
-    place_file["Ratings"][app_id] = {"Rating": rating, "DoesOwn": True}
+    place["ratings"][app_id] = {"rating": rating, "owned": True}
     app[rating and "AppLikes" or "AppDislikes"] += 1
 
-    (Path("data/apps") / f"{app_id}.json").write_bytes(json.dumps(app))
-    (Path("data/places") / f"{req.headers.get("Roblox-Id")}.json").write_bytes(json.dumps(place_file))
+    db.set(app_id, app, db.APPS)
+    db.set(req.headers.get("Roblox-Id"), place, db.PLACES)
 
     return JSONResponse({
             "code": 200,
@@ -122,13 +182,10 @@ async def rate_app(req: Request, app_id: str, payload: RatingPayload):
 
 @app.post("/install/{app_id}")
 async def install_app(req: Request, app_id: str):
-    place_file = (Path("data/places") / f"{req.headers.get("Roblox-Id")}.json")
+    place = db.get(req.headers.get("Roblox-Id"), db.PLACES)
 
-    if not place_file.is_file():
-        (Path("data/places") / f"{req.headers.get("Roblox-Id")}.json").write_bytes(json.dumps({"Installs": [],"Ratings":{}}))
-        place_file = (Path("data/places") / f"{req.headers.get("Roblox-Id")}.json")
-
-    place_file = json.loads(place_file.read_text())
+    if not place:
+        place = json.dumps({"apps": [],"ratings":{}})
 
     app = request_app(app_id)
     if not app:
@@ -138,19 +195,19 @@ async def install_app(req: Request, app_id: str):
             "user_facing_message": "Could not find that app. Was it deleted?"
         }, status_code=404)
     
-    if app_id in place_file["Installs"]:
+    if app_id in place["apps"]:
         return JSONResponse({
             "code": 400,
             "message": "bad-request",
             "user_facing_message": "This resource may only be used once."
         }, status_code=400)
     
-    place_file["Installs"].append(app_id)
+    place["apps"].append(app_id)
     app["AppDownloadCount"] += 1
     app["AppInstalls"].append(req.headers.get("Roblox-Id"))
 
-    (Path("data/apps") / f"{app_id}.json").write_bytes(json.dumps(app))
-    (Path("data/places") / f"{req.headers.get("Roblox-Id")}.json").write_bytes(json.dumps(place_file))
+    db.set(app_id, app, db.APPS)
+    db.set(req.headers.get("Roblox-Id"), place, db.PLACES)
 
     return JSONResponse({
             "code": 200,
@@ -160,11 +217,37 @@ async def install_app(req: Request, app_id: str):
 
 @app.get("/list")
 async def app_list():
-    return JSONResponse((Path("data") / "list.json").read_text(), status_code=200)
+    apps = db.get_all(db.APPS)
+    final = []
+    t = time()
+
+    for app in apps:
+        app = app["data"]
+        print(app)
+        final.append(
+            {
+                "AppName": app["AppName"],
+                "AppShostDescription": app["AppShortDescription"],
+                "AppDownloadCount": app["AppDownloadCount"],
+                "AppRating": app["AppLikes"] / (app["AppLikes"] + app["AppDislikes"]),
+                "AppDeveloperID": app.get("AppDeveloperID", 0),
+                "UpdatedAt": app["AppUpdatedUnix"],
+                "AppID": app["AdministerMetadata"]["AdministerID"],
+                "AppType": app["AppType"]
+            }
+        )
+
+    final.append({"processed_in": time() - t})
+
+    return JSONResponse(final, status_code=200)
 
 @app.get("/query/{search}")
-async def search():
-    pass
+async def search(req: Request, search: str):
+    return db.raw_find_all({"AppName": search.lower()}, db.APPS)
+
+@app.get("/misc-api/donation-passes")
+async def donation_passes():
+    return JSONResponse(content=json.loads((Path("data") / "donations.json").read_text()), status_code=200)
 
 @app.get("/misc-api/prominent-color")
 async def get_prominent_color(image_url: str):
@@ -186,6 +269,7 @@ async def proxy_request(subdomain: str, path: str, request: Request):
     headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
     headers["host"] = f"{subdomain}.roblox.com"
 
+    headers.pop("roblox-id", None)
     params = dict(request.query_params)
 
     async with httpx.AsyncClient() as client:
@@ -205,7 +289,4 @@ async def proxy_request(subdomain: str, path: str, request: Request):
         elif request.method == "OPTIONS":
             response = await client.options(target_url, headers=headers, params=params)
 
-    print(response.content)
-
-    return response.content
-
+    return Response(content=response.content, status_code=response.status_code, headers=dict(response.headers))
---
+++ modules/database.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2023-2024 Codelet Team (pyxfluff / iiPythonx)
+# Repurposed bc it's running on the same VM
+
+# Modules
+import time
+import platform
+import contextlib
+from typing import Any, List, Dict
+
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+# Main database class
+class Database(object):
+    def __init__(self) -> None:
+
+        # Reference tables
+        for db_item in ["error_refs", "apps", "proxy_cache", "api_keys", "places"]:
+            setattr(self, db_item.upper(), db_item)
+
+        # Connect to Mongo
+        client = MongoClient("zen" in platform.release() and "mongodb://mail.iipython.dev:27017" or "mongodb://127.0.0.1:27017", serverSelectionTimeoutMS = 3000)
+
+        self.db = client["administer"]  # administer database to hopefully not mess with codelet dev
+
+        # Test connection
+        try:
+            client.admin.command("ping")
+
+        except ConnectionFailure as e:
+            print(f"Failed to connect to MongoDB within the required timeframe! Is Mongo running? Aborting startup...")
+            raise e
+
+    def set(self, key: str | int, value: Any, db: str) -> None:
+        assert isinstance(key, (str, int)), "key must be a string (integers accepted)!"
+        assert isinstance(db, str), "db must be a string!"
+
+        db = self.db[db]
+
+        # Fetch active document
+        internal_document = {"administer_id": str(key)}
+        active_document = db.find_one(internal_document)
+        if active_document is not None:
+            return db.update_one(
+                {"_id": active_document["_id"]},
+                {"$set": {"data": value}}
+            )
+
+        return db.insert_one(internal_document | {"data": value})
+
+    def set_batch(self, items: Dict[str | int, dict], db: str) -> None:
+        assert isinstance(items, dict), "items must be a dict!"
+        assert isinstance(db, str), "db must be a string!"
+
+        for k, v in items.items():
+            self.set(k, v, db)
+
+    def get(self, key: str | int, db: str) -> dict | None:
+        assert isinstance(key, (str, int)), "key must be a string (integers accepted)"
+        assert isinstance(db, str), "db must be an attr of db"
+
+        document = self.db[db].find_one({"administer_id": str(key)})
+        return document and document["data"]
+
+    def find(self, identifier: dict, db: str) -> str | None:
+        assert isinstance(identifier, dict), "identifier must be a dict!"
+        assert isinstance(db, str), "db must be a string!"
+
+        document = self.db[db].find_one({f"data.{k}": v for k, v in identifier.items()})
+        return document and document["administer_id"]
+
+    def delete_old(self, difference: int) -> int:
+        current_time, old_keys = time.time(), []
+        for document in self.db[self.CODES].find({}):
+            created_at = document["data"].get("CreatedAt")
+            if not isinstance(created_at, int):
+                continue
+
+            elif created_at < current_time - difference:
+                old_keys.append(document["administer_id"])
+
+        if old_keys:
+            self.bulk_delete(old_keys, self.CODES)
+
+        return len(old_keys)
+
+    def delete(self, key: str | int, db: str) -> None:
+        assert isinstance(key, (str, int)), "key must be a string or integer!"
+
+        self.db[db].delete_one({"administer_id": str(key)})
+
+    def bulk_delete(self, keys: List[str | int], db: str) -> None:
+        assert isinstance(keys, list), "keys must be a list! (try using db.delete())"
+
+        self.db[db].delete_many({"administer_id": {"$in": keys}})
+    
+    def get_all(self, db: str) -> dict:
+       return list(self.db[db].find())
+    
+    def get_all_paged(self, db: str, limit: int, page: int) -> List[dict]:
+        return [d["data"] for d in list(self.db[db].aggregate([{"$skip": limit * (page - 1)}, {"$limit": limit}]))]
+
+    # Wrappers for raw MongoDB operations
+    def raw_insert(self, item: dict, db: str) -> None:
+        self.db[db].insert_one(item)
+
+    def raw_find(self, identifier: dict, db: str) -> dict:
+        return self.db[db].find_one(identifier)
+    
+    def raw_del(self, identifier: dict, db: str) -> dict:
+        return self.db[db].delete_one(identifier)
+    
+    def raw_purge(self, identifier: dict, db: str) -> dict:
+        return self.db[db].delete_many(identifier)
+
+
+    def raw_find_all(self, identifier: dict, db: str) -> List[dict]:
+        return self.db[db].find(identifier)
+
+    # Context managers
+    @contextlib.contextmanager
+    def as_user(self, user_id: int) -> None: # type: ignore
+        user_data = self.get(user_id, self.PLAYERS) or {}  # Must always be a dictionary, do not edit
+        yield user_data
+        self.set(user_id, user_data, self.PLAYERS)
+
+# Initialize db
+db = Database() 
