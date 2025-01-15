c328b90db97298e1ea4f070698d84a9523a59219
pyxfluff <git@notpyx.me>
2025-01-15 13:47:53 -0500

    aaa

--- build/lib/helpers.py
+++
@@ -1,5 +0,0 @@
-# pyxfluff 2024
-from .database import db
-
-def request_app(app_id):
-    return db.get(app_id, db.APPS)
--- build/lib/models/RatingPayload.py
+++
@@ -1,6 +0,0 @@
-# pyxfluff 2024
-
-from pydantic import BaseModel
-
-class RatingPayload(BaseModel):
-    Rating: int
